6c7897bed92b73c4689cdb1ee5b029393c23d62b
Alejandro Uribe <jauriberamirez@gmail.com>
2023-03-21 23:33:05 -0300

    Day 6

--- src/main/java/com/leetcode/level1/day5/LongestPalindrome.java
+++ src/main/java/com/leetcode/level1/day5/LongestPalindrome.java
@@ -3,10 +3,19 @@ package com.leetcode.level1.day5;
 public class LongestPalindrome {
 
   public static int longestPalindrome(String s) {
+    int[] count = new int[128];
     int longestpalindrome = 0;
 
-    // FIXME: Code something here!
+    for (char c : s.toCharArray()) {
+      count[c]++;
+    }
 
+    for (int v : count) {
+      longestpalindrome += v / 2 * 2;
+      if (longestpalindrome % 2 == 0 && v % 2 == 1) {
+        longestpalindrome++;
+      }
+    }
     return longestpalindrome;
   }
 
---
+++ src/main/java/com/leetcode/level1/day6/NaryTreePreorderTraversal.java
@@ -0,0 +1,37 @@
+package com.leetcode.level1.day6;
+
+import com.leetcode.level1.utils.Node;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaryTreePreorderTraversal {
+
+  /*
+  Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+  Nary-Tree input serialization is represented in their level order traversal.
+  Each group of children is separated by the null value
+   */
+  List<Integer> preordered = new ArrayList<>();
+
+  public List<Integer> preorder(Node root) {
+    if (root == null) {
+      return preordered;
+    }
+
+    depthFirstSearch(root);
+
+    return preordered;
+  }
+
+  public void depthFirstSearch(Node node) {
+    if (node.children == null) {
+      return;
+    }
+    preordered.add(node.val);
+
+    for (Node children : node.children) {
+      depthFirstSearch(children);
+    }
+  }
+}
