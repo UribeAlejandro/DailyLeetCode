f847f62702fc67144d4e1bc589b00277dfbe2638
Alejandro Uribe <jauriberamirez@gmail.com>
2024-02-19 14:03:28 -0300

    Added - Two Sum with testing

--- src/main/java/com/leetcode/selected/TwoSum.java
+++ src/main/java/com/leetcode/selected/TwoSum.java
@@ -5,28 +5,40 @@ import static java.lang.Math.abs;
 public class TwoSum {
 
   public static int[] twoSumBruteForce(int[] nums, int target) {
+    int[] result = new int[2];
 
     for (int i = 0; i < nums.length; i++) {
       for (int j = i + 1; j < nums.length; j++) {
         int complement = target - nums[i];
         if (complement == nums[j]) {
-          return new int[]{i, j};
+          result[0] = i + 1;
+          result[1] = j + 1;
+          return result;
         }
       }
     }
-    return new int[]{};
+    return result;
   }
 
-  // TODO: Implement using hash table
-//  public static int[] twoSum(int[] nums, int target) {
-//    int curr;
-//    int next = 1;
-//
-//    if (nums == null) {
-//      return new int[]{};
-//    }
-//
-//    return new int[]{};
-//  }
+  public static int[] twoSumTwoPointers(int[] numbers, int target) {
+    int n = numbers.length;
+    int pointer1 = 0;
+    int pointer2 = n-1;
+    int[] result = new int[2];
+
+    while (pointer1 < pointer2) {
+      int sum = numbers[pointer1] + numbers[pointer2];
+      if (sum == target) {
+        result[0] = pointer1 + 1;
+        result[1] = pointer2 + 1;
+        break;
+      } else if (sum < target) {
+        pointer1++;
+      } else {
+        pointer2--;
+      }
+    }
+    return result;
+  }
 
 }
--- src/test/java/com/leetcode/selected/TwoSumTest.java
+++ src/test/java/com/leetcode/selected/TwoSumTest.java
@@ -10,14 +10,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class TwoSumTest {
 
-  public static Stream<Arguments> TwoSumTest() {
+  public static Stream<Arguments> getSource() {
     return Stream.of(
-        Arguments.of(new int[]{-1, -2, -3, -4, -5}, -8, new int[]{2, 4}),
-        Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
-        Arguments.of(new int[]{0, 4, 3, 0}, 0, new int[]{0, 3}),
-        Arguments.of(new int[]{3, 2, 4}, 6, new int[]{1, 2}),
-        Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1}),
-        Arguments.of(new int[]{1, 1}, 5, new int[]{})
+        Arguments.of(new int[]{-1, -2, -3, -4, -5}, -8, new int[]{3, 5}),
+        Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{1, 2}),
+        Arguments.of(new int[]{0, 4, 3, 0}, 0, new int[]{1, 4}),
+        Arguments.of(new int[]{3, 2, 4}, 6, new int[]{2, 3}),
+        Arguments.of(new int[]{3, 3}, 6, new int[]{1, 2}),
+        Arguments.of(new int[]{1, 1}, 5, new int[2])
     );
   }
 
@@ -25,10 +25,17 @@ class TwoSumTest {
   void setUp() {
   }
 
-  @MethodSource
+  @MethodSource("getSource")
   @ParameterizedTest
   public void TwoSumTest(int[] nums, int target, int[] expected) {
     int[] actual = TwoSum.twoSumBruteForce(nums, target);
+    assertArrayEquals(expected, actual);
+  }
+
+  @MethodSource("getSource")
+  @ParameterizedTest
+  public void TwoSumTwoPointersTest(int[] nums, int target, int[] expected) {
+    int[] actual = TwoSum.twoSumTwoPointers(nums, target);
     assertArrayEquals(expected, actual);
   }
 }
